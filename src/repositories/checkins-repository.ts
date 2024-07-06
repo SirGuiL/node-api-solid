fe48740b174d51b6665c251d6abb6bb26ace360e
Guilherme Lima <guibiel-10@hotmail.com>
2024-07-06 19:04:49 -0300

    test: create test to validate if has checkin in the same day

--- src/repositories/checkins-repository.ts
+++ src/repositories/checkins-repository.ts
@@ -2,4 +2,5 @@ import { CheckIn, Prisma } from '@prisma/client'
 
 export interface CheckInsRepository {
   create: (data: Prisma.CheckInUncheckedCreateInput) => Promise<CheckIn>
+  findByUserIdOnDay: (userId: string, day: Date) => Promise<CheckIn | null>
 }
--- src/repositories/in-memory/in-memory-checkins-repository.ts
+++ src/repositories/in-memory/in-memory-checkins-repository.ts
@@ -5,6 +5,18 @@ import { randomUUID } from 'node:crypto'
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public checkIns: CheckIn[] = []
 
+  async findByUserIdOnDay(userId: string, day: Date) {
+    const checkInOnSameDate = this.checkIns.find(
+      (checkIn) => checkIn.user_id === userId,
+    )
+
+    if (!checkInOnSameDate) {
+      return null
+    }
+
+    return checkInOnSameDate
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const { gym_id, user_id, validated_at } = data
 
