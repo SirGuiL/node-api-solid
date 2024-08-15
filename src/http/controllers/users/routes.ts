1b80116ec1b9e41efd49e71ef6006fd0abb1ba89
SirGuiL <guibiel-10@hotmail.com>
2024-08-15 00:55:22 -0300

    chore: remove space

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -7,7 +7,6 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 export const userRoutes = async (app: FastifyInstance) => {
   app.post('/users', register)
-
   app.post('/sessions', authenticate)
 
   // authenticated
